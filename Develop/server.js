d135de4fa63b1cf768d3c88deea1c5e859e447b4
Daniel Altman <Daniel.Altman4@gmail.com>
2020-10-14 23:17:01 -0700

    adding server function

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,47 @@
+/* ---- NPM Imports ---- */
 const express = require("express");
-const logger = require("morgan");
+const mongojs = require("mongojs");
 const mongoose = require("mongoose");
-const compression = require("compression");
+const logger = require("morgan");
+const path = require("path");
 
-const PORT = 3000;
-
+//Set reference to express server.
 const app = express();
-
+/* ---- Middleware ---- */
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Set static to public so all file extensions can begin from public.
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
+/* ---- Database ---- */
+//Connect to database.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+//Set up Mongo database.
+const databaseUrl = process.env.MONGODB_URI || "budget";
+const collections = ["budget"];
+
+//Set reference to our database.
+const db = mongojs(databaseUrl, collections);
+
+//If there is an  error with the database, throw it.
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+//Routes
 app.use(require("./routes/api.js"));
 
+/* ---- Server set up ---- */
+//Set PORT value.
+const PORT = process.env.PORT || 3550;
+
+//Establish connection to the app.
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Application running on PORT ${PORT}`);
 });
